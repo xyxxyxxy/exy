79a6797bac40c24f8718ba1655b2afcb9c70947b
xyxxyxxy <7705456+xyxxyxxy@users.noreply.github.com>
2024-02-27 22:51:05 +0100

    reflect Discord status with tray icon

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,8 +1,13 @@
 import { BrowserWindow, Menu, MenuItem, MenuItemConstructorOptions, Tray, app } from 'electron'
-import { fromEvent } from 'rxjs'
+import { combineLatest, fromEvent, map } from 'rxjs'
 import { name } from '../../package.json'
 import { config$, toggleMediaServerActive } from './core/stores/config'
 import icon from '../../resources/icon.png?asset'
+import iconActive from '../../resources/icon-active.png?asset'
+import iconInactive from '../../resources/icon-inactive.png?asset'
+import { activity$ } from './core/activity'
+import { connectionStatus$ } from './core/discord'
+import { ConnectionStatus } from './core/discord/types'
 
 export function initTray(mainWindow: BrowserWindow): void {
   const tray = new Tray(icon)
@@ -12,6 +17,28 @@ export function initTray(mainWindow: BrowserWindow): void {
   // Show window on tray click.
   fromEvent(tray, 'click').subscribe(() => mainWindow.show())
 
+  // Reflect playing state/Discord status with tray icon.
+  combineLatest([activity$, connectionStatus$]).subscribe(([activity, discordStatus]) => {
+    let image = icon
+    let toolTip = name
+
+    // Discord not ready.
+    if (discordStatus !== ConnectionStatus.Ready) {
+      image = iconInactive
+      toolTip += ` - Discord disconnected`
+    }
+
+    // Discord ready and activity present.
+    if (discordStatus === ConnectionStatus.Ready && activity) {
+      image = iconActive
+      toolTip += ` - Playing ${activity.title}`
+    }
+
+    tray.setImage(image)
+    tray.setToolTip(toolTip)
+  })
+
+  // Build tray men.
   config$.subscribe((config) => {
     const template: Array<MenuItemConstructorOptions | MenuItem> = []
 
