07cf1c6ae0ac60d55b0b11ffbe1416ae83f3ba69
xyxxyxxy <7705456+xyxxyxxy@users.noreply.github.com>
2024-03-02 23:10:34 +0100

    add guard for ignored activity types

--- src/main/core/activity/index.ts
+++ src/main/core/activity/index.ts
@@ -1,14 +1,25 @@
 import log from 'electron-log'
 import { Activity } from './types'
-import { BehaviorSubject, Observable, combineLatest, map, tap } from 'rxjs'
-import { configActivity$ } from '../stores/config'
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  map,
+  shareReplay,
+  tap,
+  withLatestFrom
+} from 'rxjs'
+import { configActivity$, configIgnoredItemTypes$ } from '../stores/config'
 import { mediaServerMainActivity$ } from '../media-server'
-import { addConfigExtras } from './utils'
+import { addConfigExtras, isIgnoredActivity } from './utils'
 
 const logger = log.scope('activity')
 
 const activitySubject: BehaviorSubject<Activity | null> = new BehaviorSubject<Activity | null>(null)
-export const activity$: Observable<Activity | null> = activitySubject.asObservable()
+export const activity$: Observable<Activity | null> = activitySubject.asObservable().pipe(
+  tap((activity) => logger.debug(`New activity:`, activity)),
+  shareReplay(1)
+)
 
 // Subscribe to activity providers.
 combineLatest([
@@ -24,14 +35,19 @@ combineLatest([
       // avoid mutating the same object twice on config event.
       const clone = Object.assign({}, activity)
       return addConfigExtras(clone, config)
+    }),
+    withLatestFrom(configIgnoredItemTypes$),
+    // Last guard against ignored activity types.
+    map(([activity, ignored]) => {
+      if (activity && isIgnoredActivity(activity, ignored)) {
+        logger.warn(`Ignored activity type emitted by source. Will be ignored.`)
+        return null
+      }
+      return activity
     })
   )
-  .subscribe((mediaServerActivity) => activitySubject.next(mediaServerActivity))
 
-// Logging of activity.
-activitySubject.subscribe((activity) => {
-  logger.debug(`New activity:`, activity)
-})
+  .subscribe((mediaServerActivity) => activitySubject.next(mediaServerActivity))
 
 export function clearActivity(): void {
   activitySubject.next(null)
