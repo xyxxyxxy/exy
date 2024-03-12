09525e2c123dd52744acd961cb9c18bfdeba3529
xyxxyxxy <7705456+xyxxyxxy@users.noreply.github.com>
2024-03-12 08:46:19 +0100

    resolve last TODOs

--- src/main/core/activity/index.ts
+++ src/main/core/activity/index.ts
@@ -1,15 +1,23 @@
 import log from 'electron-log'
 import { Activity } from './types'
-import { BehaviorSubject, Observable, map, shareReplay, tap, withLatestFrom } from 'rxjs'
+import {
+  BehaviorSubject,
+  Observable,
+  distinctUntilChanged,
+  map,
+  shareReplay,
+  tap,
+  withLatestFrom
+} from 'rxjs'
 import { config$ } from '../stores/config'
 import { mediaServerMainActivity$ } from '../media-server'
 import { isIgnoredType } from './utils'
 
 const logger = log.scope('activity')
 
-const activitySubject: BehaviorSubject<Activity | null> = new BehaviorSubject<Activity | null>(null)
-export const activity$: Observable<Activity | null> = activitySubject.asObservable().pipe(
-  // TODO Emits null twice on startup
+const activitySource: BehaviorSubject<Activity | null> = new BehaviorSubject<Activity | null>(null)
+export const activity$: Observable<Activity | null> = activitySource.asObservable().pipe(
+  distinctUntilChanged(),
   tap((activity) => logger.debug(`New activity:`, activity)),
   shareReplay(1)
 )
@@ -32,8 +40,8 @@ mediaServerMainActivity$
     })
   )
 
-  .subscribe((mediaServerActivity) => activitySubject.next(mediaServerActivity))
+  .subscribe((mediaServerActivity) => activitySource.next(mediaServerActivity))
 
 export function clearActivity(): void {
-  activitySubject.next(null)
+  activitySource.next(null)
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -103,13 +103,6 @@ app.whenReady().then(() => {
   })
 
   createWindow()
-
-  // TODO Handle macOS dock
-  // app.on('activate', function () {
-  //   // On macOS it's common to re-create a window in the app when the
-  //   // dock icon is clicked and there are no other windows open.
-  //   if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  // })
 })
 
 config$
